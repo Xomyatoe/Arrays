783450a6fa9f0bb41edf672ba95d70d491e18b69
Seva <Seva@DESKTOP-PLC715R>
2024-02-05 23:38:08 +0500

    Introduction to Arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,4 +3,15 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
+	const int SIZE = 5;
+	int arr[SIZE];
+	
+	//arr[2] = 2048;
+	//cout << arr[2] << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
