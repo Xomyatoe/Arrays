5119e238581c9fe4fd4dd44085f30c35bdf07e02
Seva <Seva@DESKTOP-PLC715R>
2024-02-07 17:08:03 +0500

    homework

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,15 +3,63 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Arrays" << endl;
+	//cout << "Hello Arrays" << endl;
+	//const int SIZE = 5;
+	//int arr[SIZE];
+	//
+	////arr[2] = 2048;
+	////cout << arr[2] << endl;
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cout << arr[i] << "\t";
+	//}
+	//cout << endl;
+
 	const int SIZE = 5;
-	int arr[SIZE];
-	
-	//arr[2] = 2048;
-	//cout << arr[2] << endl;
-	for (int i = 0; i < SIZE; i++)
+	int i, m[SIZE]; // Объявляем целочисленную переменную и один массив.
+
+	for (i = 0; i < 5; i++)
 	{
-		cout << arr[i] << "\t";
+		cin >> m[i]; // Вводим с клавиатуры значение ячейки массива
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		cout << "m[" << i << "]= " << m[i] << "\n";
 	}
 	cout << endl;
+	///
+	for (i = 4; i  >= 0; i--)
+	{
+		cout << "m[" << i << "]= " << m[i] << "\n";
+	}
+	cout << endl;
+	///
+	int sum = 0;
+	for ( i = 0; i < 5; i++)
+	{
+  sum = sum + m[i];
+	}
+	cout <<"Сумма массивов = " << sum;
+	cout << endl;
+	int sum1 = 0;
+	for (i = 0; i < 5; i++)
+	{
+		sum1 = sum1 + m[i];
+	}
+	cout << "Среднее арифметическое элементов массива = " << sum / 5;
+	cout << endl;
+	int max_el = m[0];
+	int min_el = m[0];
+	if (max_el > m[i])
+	{
+		max_el = i;
+	}
+	if (min_el < m[i])
+	{
+		min_el = i;
+	}
+	cout << "Максимальное значение = " << max_el << "\n";
+	cout << "Минимальное значение = " << min_el << "\n";
+	
 }
